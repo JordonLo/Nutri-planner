42d3aa0467200945fa33cfd9d0943957ecf62ebf
JordonLo <j.lor99@yahoo.com>
2022-12-19 21:40:13 -0800

    pushing daily task progress

--- assets/script.js
+++ assets/script.js
@@ -49,16 +49,7 @@ calendar += '</tr></table>';
 // Insert the calendar into the div with the id "calendar"
 document.getElementById('calendar').innerHTML = calendar;
 
-// Add an event listener to each button in the calendar
-var clickedDate;
-var buttons = document.querySelectorAll('button');
-buttons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    clickedDate=this.getAttribute('data-date');
-    console.log(clickedDate);
-  });
-});
-
+// Jordon changes (local storage for nutrition and calories)
 var nutritionInput = document.querySelector("#Nutrition");
 var caloriesInput = document.querySelector("#Calories");
 var saveButton = document.querySelector("#save")
@@ -75,3 +66,81 @@ localStorage.setItem("user", JSON.stringify(user));
 
 });
 
+var dailytaskInput = document.querySelector("#dailytask-text");
+var dailytaskList = document.querySelector("#dailytask-list");
+var dailyTaskForm = document.querySelector("#dailyTask-form");
+var dailytaskCountSpan = document.querySelector("#dailyTask-count");
+var dailyTasks = [];
+
+function renderdailyTasks() {
+  
+  dailytaskList.innerHTML = "";
+  dailytaskCountSpan.textContent = dailyTasks.length;
+  
+  for (var i = 0; i < dailyTasks.length; i++) {
+    var dailyTask = dailyTasks[i];
+
+    var li = document.createElement("li");
+    li.textContent = dailyTask;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Complete ✔️";
+
+    li.appendChild(button);
+    dailytaskList.appendChild(li);
+  }
+}
+
+function init() {
+ 
+  var storedDailytasks = JSON.parse(localStorage.getItem("dailyTasks"));
+
+  if (storedDailytasks !== null) {
+    dailyTasks = storedDailytasks;
+  }
+
+  renderdailyTasks();
+}
+
+function storeDailytasks() {
+  localStorage.setItem("dailyTasks", JSON.stringify(dailyTasks));
+}
+
+dailyTaskForm.addEventListener("Sumbit", function(event) {
+  event.preventDefault();
+
+  var dailytaskInput = dailytaskInput.value.trim();
+
+  // Return from function early if submitted todoText is blank
+  if (dailytaskInput === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  dailyTasks.push(dailytaskInput);
+  dailytaskInput.value = "";
+
+  // Store updated todos in localStorage, re-render the list
+  storeDailytasks();
+  renderdailyTasks();
+});
+
+// Add click event to todoList element
+dailytaskList.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // Checks if element is a button
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    dailyTasks.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+    storeDailytasks();
+    renderdailyTasks();
+  }
+});
+
+// Calls init to retrieve data and render it to the page on load
+init()
